0de64d3ed1a0b6279103fb77c40c3f147b4a0a2f
NNTruong <nntruong.hvktqs@gmail.com>
2020-03-10 17:37:20 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+var NodeMailer = require('./helper/node-mailer')
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const app = express();
+
+require('dotenv').config()
+const PORT = process.env.PORT || 3000;
+
+//setting view engine
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+//Static file
+app.use('/public', express.static(path.join(__dirname,'public')))
+
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended : false }))
+app.use(bodyParser.json())
+
+app.get('/', (req,res) => {
+    res.render('contact', {layout : false})
+})
+
+app.post('/send', (req,res) => {
+    NodeMailer(req,res);
+    res.send('success')
+    //res.render('contact', {layout : false} , {msg: 'Email has been seen...'} )
+})
+
+app.listen(PORT , () => {
+    console.log(`Server is running port ${PORT}`)
+})
---
+++ helper/node-mailer.js
@@ -0,0 +1,43 @@
+const nodemailer = require('nodemailer')
+require('dotenv').config()
+async function NodeMailer(req, res) {
+   
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth : {
+            user : 'nntruong020699@gmail.com',
+            pass : process.env.password
+        }
+    })
+    
+    const output = `
+        <p>You have a new contact request</p>
+        <h3>Contact details</h3>
+        <ul>
+            <li>Name : ${req.body.name}</li>
+            <li>Company : ${req.body.company}</li>
+            <li>Email : ${req.body.email}</li>
+            <li>Phone : ${req.body.phone}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.message}</p>
+    `;
+    // send mail with defined transport object
+    let mailOptions = {
+        from: '"Node mailer contact" <nntruong020699@gmail.com>', // sender address
+        to: "anhtt@media-one.vn", // list of receivers
+        subject: "Node Contact request", // Subject line
+        text: "Hello world?", // plain text body
+        html: output // html body
+    };
+
+    transporter.sendMail(mailOptions, (err,info) => {
+        if(err)
+            console.log(err)
+        else
+            console.log(info)
+    })
+
+}
+
+module.exports = NodeMailer
